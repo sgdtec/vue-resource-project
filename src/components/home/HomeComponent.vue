635bb214709c99d7ae83c71129da95944ab9693e
Ademir Bastiani <ademir@phipasa.local>
2021-02-01 15:27:59 -0300

    First Commit

---
+++ src/components/home/HomeComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>{{tilte}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            title: 'Home Page do Site'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/products/ProductComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h1>{{title}}</h1>  
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            title: 'Lista de Produtos'
+        }
+    },
+    created () {
+        this.getProducts()
+    },
+    methods: {
+        getProducts () {         
+            this.$http.get('http://localhost:8000/api/v1/products')
+                .then(response => {
+                    console.log(response)
+                }, error => {
+                    console.log(error)
+                })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
